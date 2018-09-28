c62cf1d6431bb8753d8670b0377d70d7a4db1e9d
Andrzej Pepłowski <pirx555@gmail.com>
2018-09-28 11:42:49 +0200

    Divided into three pages and added menu.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const menuBtn1 = document.getElementById("menu-btn-1");
-const menuBtn2 = document.getElementById("menu-btn-2");
-const menuBtn3 = document.getElementById("menu-btn-3");
+const menuBtnHome = document.getElementById("menu-btn-home");
+const menuBtnWorks = document.getElementById("menu-btn-works");
+const menuBtnContact = document.getElementById("menu-btn-contact");
 
 const contentHome = document.getElementById("home");
 const contentWorks = document.getElementById("works");
@@ -8,46 +8,46 @@ const contentContact = document.getElementById("contact");
 
 const main = document.querySelector("main");
 
-menuBtn1.onclick = () => {
-  menuBtn1.classList.add("menu-btn-selected");
-  menuBtn2.classList.remove("menu-btn-selected");
-  menuBtn3.classList.remove("menu-btn-selected");
+menuBtnHome.onclick = () => {
+  menuBtnHome.classList.add("menu-btn-selected");
+  menuBtnWorks.classList.remove("menu-btn-selected");
+  menuBtnContact.classList.remove("menu-btn-selected");
 
   contentHome.classList.remove("hide");
   contentWorks.classList.add("hide");
   contentContact.classList.add("hide");
 
-  main.classList.add("nav-style");
-  main.classList.remove("main-style");
-}
+  // main.classList.add("nav-style");
+  // main.classList.remove("main-style");
+};
 
-menuBtn2.onclick = () => {
-  menuBtn2.classList.add("menu-btn-selected");
-  menuBtn1.classList.remove("menu-btn-selected");
-  menuBtn3.classList.remove("menu-btn-selected");
+menuBtnWorks.onclick = () => {
+  menuBtnWorks.classList.add("menu-btn-selected");
+  menuBtnHome.classList.remove("menu-btn-selected");
+  menuBtnContact.classList.remove("menu-btn-selected");
 
   contentHome.classList.add("hide");
   contentWorks.classList.remove("hide");
   contentContact.classList.add("hide");
 
-  main.classList.remove("nav-style");
-  main.classList.add("main-style");
-}
+  // main.classList.remove("nav-style");
+  // main.classList.add("main-style");
+};
 
-menuBtn3.onclick = () => {
-  menuBtn3.classList.add("menu-btn-selected");
-  menuBtn1.classList.remove("menu-btn-selected");
-  menuBtn2.classList.remove("menu-btn-selected");
+menuBtnContact.onclick = () => {
+  menuBtnContact.classList.add("menu-btn-selected");
+  menuBtnHome.classList.remove("menu-btn-selected");
+  menuBtnWorks.classList.remove("menu-btn-selected");
 
   contentHome.classList.add("hide");
   contentWorks.classList.add("hide");
   contentContact.classList.remove("hide");
 
-  main.classList.add("main-style");
-  main.classList.remove("nav-style");
+  // main.classList.add("main-style");
+  // main.classList.remove("nav-style");
 
-  setTimeout(() => {
-    main.classList.add("nav-style");
-    main.classList.remove("main-style");
-  }, 10);
-}
+  // setTimeout(() => {
+  //   main.classList.add("nav-style");
+  //   main.classList.remove("main-style");
+  // }, 10);
+};
